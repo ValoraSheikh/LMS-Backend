11badb5092e3d4ff012c86296a1d9840ad56245c
Aman Sheikh <haarishsheikh04@gmail.com>
2025-09-17 12:11:06 +0530

    Enhance security and performance by adding middleware and dependencies

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import express from 'express'
-import morgan from 'morgan'
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import cors from 'cors';
+import xssClean from 'xss-clean';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
 
 dotenv.config()
-console.log(process.env);
-
 const app = express();
 const PORT = process.env.PORT;
+
+//Global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  message: "Too many request from this IP, please try again",
+})
+
+// Security Middleware
+app.use("/api", limiter);
+app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use(hpp());
+app.use(xssClean());
+
 
 if(process.env.NODE_ENV === "development"){
   app.use(morgan("dev"));
@@ -15,17 +38,34 @@ if(process.env.NODE_ENV === "development"){
 // Body parser midlleware
 app.use(express.json({ limit: "10kb"}));
 app.use(express.urlencoded({ extended: true, limit: "10kb"}))
+app.use(cookieParser());
 
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error";
+    message: err.message || "Internal Server Error",
     status: "error",
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   })
   
 })
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "User-Agent",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials"
+  ]
+}));
+
 
 
 app.use((req, res) => {
