64395683a9436f3ad20223837564305e4a473bbf
Aman Sheikh <haarishsheikh04@gmail.com>
2025-09-19 15:54:44 +0530

    Added validate Signup for signup router

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,17 +8,22 @@ import {
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth.middleware";
 import upload from "../utils/multer";
-
+import { validateSignUp } from "../middleware/validation.middleware";
 
 const router = express.Router();
 
 //Auth routes
-router.post("/signup", createUserAccount);
+router.post("/signup", validateSignUp, createUserAccount);
 router.post("/signin", authenticateUser);
 router.post("/signout", signOutUser);
 
 //Profile routes
 router.get("/profile", isAuthenticated, getCurrentUserProfile);
-router.patch("/profile", isAuthenticated, upload.single("avatar"),updateUserProfile);
+router.patch(
+  "/profile",
+  isAuthenticated,
+  upload.single("avatar"),
+  updateUserProfile
+);
 
 export default router;
