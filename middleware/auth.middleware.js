3225e4d9ff25c92eea856bd289a4e9fa555ff092
Aman Sheikh <haarishsheikh04@gmail.com>
2025-09-25 16:40:14 +0530

    Write restrictTo funciton in auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,4 @@
+import { User } from "../models/user.model";
 import { ApiError, catchAsync } from "./error.middleware";
 import jwt from "jwt";
 
@@ -11,8 +12,28 @@ export const isAuthenticated = catchAsync(async (req, res, next) => {
   try {
     const decoded = await jwt.verfiy(token, process.env.SECRET_KEY);
     req.id = decoded.userId;
+
+    const user = await User.findById(req.id);
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+
+    req.user = user;
+
     next();
   } catch (error) {
     throw new ApiError("JWT token error", 401);
   }
 });
+
+export const restrictTo = (...roles) => {
+  return catchAsync(async (req, _res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new ApiError(
+        "You do not have permission to perform this action",
+        403
+      );
+    }
+    next();
+  });
+};
