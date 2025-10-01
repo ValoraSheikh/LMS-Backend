e5e736b4f0c49d1aa59f100831be4f91cbfca143
Aman Sheikh <haarishsheikh04@gmail.com>
2025-10-01 11:14:07 +0530

    Implemented update lecture progress controller

--- controllers/courseProgress.controller.js
+++ controllers/courseProgress.controller.js
@@ -53,4 +53,54 @@ export const getUserCourseProgress = catchAsync(async (req, res) => {
   });
 });
 
+export const updateLectureProgress = catchAsync(async (req, res) => {
+  // TODO: Implement update lecture progress functionality
+
+  const { lectureId, courseId } = req.params;
+
+  let courseProgress = await CourseProgress.findOne({
+    user: req.id,
+    course: courseId,
+  });
+
+  if (!courseProgress) {
+    courseProgress = await CourseProgress.create({
+      user: req.id,
+      course: courseId,
+      lectureProgress: [],
+    });
+  }
+
+  const lectureIndex = courseProgress.lectureProgress.findIndex(
+    (lecture) => lecture.lecture === lectureId
+  );
+
+  if (lectureIndex !== -1) {
+    courseProgress.lectureProgress[lectureIndex].isCompleted = true;
+  } else {
+    courseProgress.lectureProgress.push({
+      lecture: lectureId,
+      isCompleted: true,
+    });
+  }
+
+  const course = await Course.findById(courseId);
+  const completedLectures = courseProgress.lectureProgress.filter(
+    (lp) => lp.isCompleted == true
+  ).length;
+
+  (courseProgress.lectureProgress == course.lectures.length) ===
+    completedLectures;
+
+  await courseProgress.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Lecture Progress updated successfully",
+    data: {
+      lectureProgress: courseProgress.lectureProgress,
+      isCompleted: courseProgress.isCompleted,
+    },
+  });
+});
 
